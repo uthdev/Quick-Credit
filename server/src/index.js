d555ee50732e390fe3bcfa6bca839bf2ffec9eff
uthdev <adelekegbolahan92@yahoo.com>
2019-05-02 10:02:04 +0100

    feature(server):setup server

---
+++ server/src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 5001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => console.log(`Quick credit server running at port ${PORT}...`));
