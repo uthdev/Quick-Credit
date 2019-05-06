1263a05b55638c7b30e24af00faf2113a5227a89
uthdev <adelekegbolahan92@yahoo.com>
2019-05-06 16:56:31 +0100

    feature(db):add user non-persistent mock db

---
+++ server/src/mocks/mockData.js
@@ -0,0 +1,46 @@
+class Data {
+  constructor() {
+    this.users = [{
+      id: 1,
+      email: 'woleadaabale@gmail.com',
+      firstname: 'Wole',
+      lastname: 'Adaabale',
+      password: 'wolebale',
+      address: 'sw1/219, isale Osi, Ibadan',
+      status: 'unverified',
+      isAdmin: false,
+    },
+    {
+      id: 2,
+      email: 'tombraddy@yahoo.com',
+      firstname: 'Tom',
+      lastname: 'Braddy',
+      password: 'Bradit##',
+      address: 'I7, oshodi-isale, lagos',
+      status: 'verified',
+      isAdmin: false,
+    }, {
+      id: 3,
+      email: 'nasirhassan04@gmail.com',
+      firstname: 'Hassan',
+      lastname: 'Nasir',
+      password: 'Nassan@',
+      address: '23, opp Total filling station, Bida, Niger State',
+      status: 'unverified',
+      isAdmin: false,
+    }, {
+      id: 4,
+      email: 'ezenwachi@gmail.com',
+      firstname: 'chima',
+      lastname: 'Ezenwa',
+      password: 'Ezechi4dgirls',
+      address: '4, umuagu village, Umuaya',
+      status: 'verified',
+      isAdmin: false,
+    }];
+  }
+}
+
+const data = new Data();
+
+export default data;
