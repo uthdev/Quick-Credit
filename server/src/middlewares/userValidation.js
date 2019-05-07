1120bd0061c5fff1de0bdc8e66caf13557ec21f5
uthdev <adelekegbolahan92@yahoo.com>
2019-05-07 05:17:47 +0100

    feature(middleware):add signin validation

--- server/src/middlewares/userValidation.js
+++ server/src/middlewares/userValidation.js
@@ -23,4 +23,24 @@ export default class AccountValidator {
       });
     });
   }
+
+  static loginValidator (req, res, next) {
+    const user = req.body;
+
+    const userProperties = {
+      email: 'required|email|max:50',
+      password: 'required|alpha_dash|min:6|max:20',
+    }
+
+    const validator = new Validator(user, userProperties, customErrorMsgs);
+
+    validator.passes(() => next());
+    validator.fails(() => {
+      const errors = validator.errors.all();
+      return res.status(400).json({
+        status: 400,
+        error: errors,
+      });
+    });
+  }
 }
