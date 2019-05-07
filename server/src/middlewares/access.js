2423c3505dfacbeaae89799a06f837f6f2948e29
uthdev <adelekegbolahan92@yahoo.com>
2019-05-07 19:49:38 +0100

    feature(middleware):add authorization middleware

---
+++ server/src/middlewares/access.js
@@ -0,0 +1,35 @@
+import Jwt from '../helpers/jwt';
+
+export default class Access {
+  static async verifyToken (req, res, next) {
+    const token = req.headers['x-access-token'];
+    if (!token) {
+      return res.status(401).json({
+        status: 401,
+        error: 'Authorization token required',
+      });
+    }
+    
+    try {
+      const decoded = await Jwt.verifyToken(token);
+      req.user = decoded;
+      next();
+    } catch (error) {
+      res.status(403).json({
+        status: 403,
+        error: 'Authorization fail! Invalid token',
+      });
+    }
+  }
+
+  static async adminAccess (req, res, next) {
+    const { isAdmin } = req.user;
+    if (isAdmin !== true) {
+      return res.status(403).json({
+        status: 403,
+        error: 'Unauthorized Access!',
+      });
+    }
+    next();
+  }
+}
