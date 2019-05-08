80e7a6be2818e47226a7a2e39c15598085679501
uthdev <adelekegbolahan92@yahoo.com>
2019-05-08 15:57:36 +0100

    feature(routes):refactor get loans routes

--- server/src/routes/loanRoutes.js
+++ server/src/routes/loanRoutes.js
@@ -2,15 +2,18 @@ import { Router } from 'express';
 import LoanController from '../controllers/loanController';
 import Access from '../middlewares/access';
 import ParamsValidator from '../middlewares/paramsValidator';
+import QueryValidator from '../middlewares/queryValidator'
 
 const { verifyToken, adminAccess } = Access;
-const { loanId} = ParamsValidator;
+const { loanId } = ParamsValidator;
+const { validateQuery } = QueryValidator
 
-const { getAll, getSpecificLoan } = LoanController;
+const { getAll, getSpecificLoan, currentRepaid } = LoanController;
 
 const loanRoute = new Router();
 
-loanRoute.get('/', verifyToken, adminAccess, getAll);
+loanRoute.get('/', verifyToken, adminAccess, getAll, validateQuery, currentRepaid);
 loanRoute.get('/:loanId', verifyToken, adminAccess, loanId, getSpecificLoan);
+
 
 export default loanRoute;
