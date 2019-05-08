0908cadd2ad89e8ca23f30d21d188587e3a33294
uthdev <adelekegbolahan92@yahoo.com>
2019-05-08 18:30:38 +0100

    feature(routes):setup endpoint route

--- server/src/routes/loanRoutes.js
+++ server/src/routes/loanRoutes.js
@@ -2,18 +2,21 @@ import { Router } from 'express';
 import LoanController from '../controllers/loanController';
 import Access from '../middlewares/access';
 import ParamsValidator from '../middlewares/paramsValidator';
-import QueryValidator from '../middlewares/queryValidator'
+import QueryValidator from '../middlewares/queryValidator';
+import LoanValidator from '../middlewares/loansValidator';
 
 const { verifyToken, adminAccess } = Access;
 const { loanId } = ParamsValidator;
-const { validateQuery } = QueryValidator
+const { validateQuery } = QueryValidator;
+const { approveLoanValidator } = LoanValidator;
 
-const { getAll, getSpecificLoan, currentRepaid } = LoanController;
+const { getAll, getSpecificLoan, currentRepaid, approveRejectLoan } = LoanController;
 
 const loanRoute = new Router();
 
 loanRoute.get('/', verifyToken, adminAccess, getAll, validateQuery, currentRepaid);
 loanRoute.get('/:loanId', verifyToken, adminAccess, loanId, getSpecificLoan);
+loanRoute.patch('/:loanId', verifyToken, adminAccess, loanId, approveLoanValidator, approveRejectLoan)
 
 
 export default loanRoute;
