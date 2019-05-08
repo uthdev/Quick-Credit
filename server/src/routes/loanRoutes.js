1d13667d180f5d536c75d8500bc401694a2e3751
uthdev <adelekegbolahan92@yahoo.com>
2019-05-08 09:50:15 +0100

    feature(route):setup get specifc loan route

--- server/src/routes/loanRoutes.js
+++ server/src/routes/loanRoutes.js
@@ -1,13 +1,16 @@
 import { Router } from 'express';
 import LoanController from '../controllers/loanController';
 import Access from '../middlewares/access';
+import ParamsValidator from '../middlewares/paramsValidator';
 
 const { verifyToken, adminAccess } = Access;
+const { loanId} = ParamsValidator;
 
-const { getAll } = LoanController;
+const { getAll, getSpecificLoan } = LoanController;
 
 const loanRoute = new Router();
 
 loanRoute.get('/', verifyToken, adminAccess, getAll);
+loanRoute.get('/:loanId', verifyToken, adminAccess, loanId, getSpecificLoan);
 
 export default loanRoute;
