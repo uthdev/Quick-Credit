754a1c526f09f6e064d2105137b46d442e4b56cb
uthdev <adelekegbolahan92@yahoo.com>
2019-05-09 13:34:02 +0100

    feature(route):add post loan application route

--- server/src/routes/loanRoutes.js
+++ server/src/routes/loanRoutes.js
@@ -8,15 +8,16 @@ import LoanValidator from '../middlewares/loansValidator';
 const { verifyToken, adminAccess } = Access;
 const { loanId } = ParamsValidator;
 const { validateQuery } = QueryValidator;
-const { approveLoanValidator } = LoanValidator;
+const { approveLoanValidator, loanApplicationValidator } = LoanValidator;
 
-const { getAll, getSpecificLoan, currentRepaid, approveRejectLoan } = LoanController;
+const { getAll, getSpecificLoan, currentRepaid, approveRejectLoan, createLoanApplication } = LoanController;
 
 const loanRoute = new Router();
 
 loanRoute.get('/', verifyToken, adminAccess, getAll, validateQuery, currentRepaid);
 loanRoute.get('/:loanId', verifyToken, adminAccess, loanId, getSpecificLoan);
 loanRoute.patch('/:loanId', verifyToken, adminAccess, loanId, approveLoanValidator, approveRejectLoan)
+loanRoute.post('/', verifyToken, loanApplicationValidator, createLoanApplication);
 
 
 export default loanRoute;
