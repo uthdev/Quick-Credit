0b1f82743850804c3fbbd247db901914e0867afe
uthdev <adelekegbolahan92@yahoo.com>
2019-05-20 21:34:36 +0100

    refactor(models):add database query to model

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -1,3 +1,5 @@
+import pool from '../database/index';
+
 class User {
   constructor(user) {
     this.id = user.id;
@@ -7,7 +9,43 @@ class User {
     this.password = user.password;
     this.address = user.address;
     this.status = user.status;
-    this.isAdmin = user.isAdmin;
+    this.isadmin = user.isadmin;
+  }
+
+  async createAccount() {
+    const status = 'unverified';
+    const queryString = `INSERT INTO users (email, firstname, lastname, password, address, status)
+    VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`;
+    const params = [this.email, this.firstname, this.lastname,
+      this.password, this.address, status];
+    try {
+      const { rows } = await pool.query(queryString, params);
+      return rows[0];
+    } catch (error) {
+      return error.message;
+    }
+  }
+
+  static async findUserByEmail(email) {
+    const queryString = 'SELECT  * FROM users WHERE email = $1';
+    const param = [email];
+    try {
+      const { rows } = await pool.query(queryString, param);
+      return rows;
+    } catch (error) {
+      return error.message;
+    }
+  }
+
+  static async updateUser (email, valueToUpdate, updateValue) {
+    const queryString = `UPDATE users SET ${valueToUpdate.replace(/"/g, '')} = $1 WHERE email = $2 RETURNING *`;
+    const params = [updateValue, email];
+    try {
+      const { rows } = await pool.query(queryString, params);
+      return rows;
+    } catch (error) {
+      return error;
+    }
   }
 }
 
